635cc9837688ab80ff70575e908bac77bda6133c
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-14 12:39:05 +0200

    uncss

--- uncss.js
+++ uncss.js
@@ -2,8 +2,8 @@ var uncss = require('uncss');
 
 var files   = ['index.html'],
     options = {
-        stylesheets  : ['css/full/app-blogs.css','css/full/cli-style.css','css/full/custom.css','css/full/gef.css','css/full/font-awesome.min.css'],
-        ignore       : [/#browser(.*)/],
+        stylesheets  : ['css/full/app-blogs.css','css/full/cli-style.css','css/full/gef.css','css/full/font-awesome.min.css','css/full/custom.css'],
+        ignore       : [/#browser(.*)/,/(.*)hover(.*)/],
     };
 
 uncss(files, options, function (error, output) {
